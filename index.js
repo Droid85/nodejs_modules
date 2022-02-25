ca219907deccf12579273311c5270e2fbc4194fe
Andrii Ostapenko <droid85@yahoo.com>
2022-02-25 15:26:37 +0200

    Added 2 npm modules cross-env and yargs

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const fs = require('fs');
 const path = require('path');
 
 let { info, warn, error } = require('./utils/logger');
+let { seek } = require('./utils/FileSeeker');
 
 info('New message');
 warn('Warning');
 error('Error!!!');
+seek();
--- utils/FileSeeker.js
+++ utils/FileSeeker.js
@@ -1,3 +1,13 @@
-function seek(target, dirPath) {
-    fs.readdir(dirPath, (err, files) => {})
+const fs = require('fs');
+const path = require('path');
+
+function seek() {
+    fs.readdir(`./utils/logger.js`, 'utf-8', (err, data) => {
+        if (err) throw err;
+        console.log(data);
+    })
+}
+
+module.exports = {
+    seek
 }
