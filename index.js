3b1c0c8147c939f86602dca8ffbbbcb810e23b92
Andrii Ostapenko <droid85@yahoo.com>
2022-03-27 17:24:48 +0300

    added seeker function

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const fs = require('fs');
 const path = require('path');
+const yargs = require('yargs');
+const FileSeeker = require("./utils/FileSeeker");
+
+const args = yargs(process.argv).argv;
 
 let { info, warn, error } = require('./utils/logger');
-let { seek } = require('./utils/FileSeeker');
 
 info('New message');
 warn('Warning');
 error('Error!!!');
-seek();
+
+if (!args.dir || !args.file) {
+    warn('--dir and --file arguments are required!')
+    process.exit(1)
+}
+
+FileSeeker.seeker(args.dir, args.file)
+    .addListener("error", err => {
+        console.error("ERROR", err.toString())
+    })
+    .addListener("success", file => {
+        info("File found ", file)
+    })
+    .addListener("data", content => {
+        info("Content", content)
+    })
--- utils/FileSeeker.js
+++ utils/FileSeeker.js
@@ -1,13 +1,34 @@
 const fs = require('fs');
 const path = require('path');
+const { promisify } = require('util');
+const EventEmitter = require('events')
+let { info, warn, error } = require('./logger');
 
-function seek() {
-    fs.readdir(`./utils/logger.js`, 'utf-8', (err, data) => {
-        if (err) throw err;
-        console.log(data);
-    })
+function seeker(dir, file) {
+    const _emitter = new EventEmitter();
+
+    const access = promisify(fs.access);
+    const readdir = promisify(fs.readdir);
+    const readfile = promisify(fs.readFile);
+
+    access(dir)
+        .then(() => readdir(dir))
+        .then(files => {
+            if (files.includes(file)) {
+                _emitter.emit("success", path.join(dir, file))
+                return readfile(path.join(dir, file), "utf-8")
+            }
+            throw new Error(error("File doesn't exist"))
+        })
+        .then(content => {
+            _emitter.emit("data", content)
+        })
+        .catch(err => {
+            _emitter.emit("error", err)
+        })
+    return _emitter;
 }
 
 module.exports = {
-    seek
+    seeker
 }
